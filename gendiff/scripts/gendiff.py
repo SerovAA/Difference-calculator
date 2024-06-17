25350c0f2020097989244c289fce0e7e6d64c233
SerovAA <serov4rtem@gmail.com>
2024-06-17 13:51:21 +0300

    stage 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,8 +5,11 @@ def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('first_file', help='first file to compare')
-    parser.add_argument('second_file', help='second file to compare')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        "-f", "--format", type=str, help="set format of output", default="plain"
+    )
 
     args = parser.parse_args()
     print(f'Comparing {args.first_file} and {args.second_file}')
