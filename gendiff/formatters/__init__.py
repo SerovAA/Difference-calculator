1b28589d8fca383fdfd7ad76a7e4c406a81668d5
SerovAA <serov4rtem@gmail.com>
2024-06-25 17:39:56 +0300

    stage 8.11

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,4 +1,4 @@
-from gendiff.formatters.stylish import format_ as format_stylish
-from gendiff.formatters.plain import format_ as format_plain
-from gendiff.formatters.json import format_ as format_json
-from gendiff.formatter import formatting
+from gendiff.formatters.stylish import format_ as format_stylish  # noqa: F401
+from gendiff.formatters.plain import format_ as format_plain  # noqa: F401
+from gendiff.formatters.json import format_ as format_json  # noqa: F401
+from gendiff.formatter import formatting  # noqa: F401
