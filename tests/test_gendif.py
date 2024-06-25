b0543ebc9d5b07a902703b5e8368dfd588a7e317
SerovAA <serov4rtem@gmail.com>
2024-06-25 22:31:35 +0300

    last stage4

--- tests/test_gendif.py
+++ tests/test_gendif.py
@@ -1,18 +1,20 @@
+import os
 import pytest
 from gendiff import generate_diff
 
 
 @pytest.fixture
 def paths():
+    fixtures_dir = "tests/fixtures"
     return {
-        "json1": "tests/fixtures/file1.json",
-        "json2": "tests/fixtures/file2.json",
-        "yml1": "tests/fixtures/file1.yml",
-        "yml2": "tests/fixtures/file2.yml",
-        "tree_json1": "tests/fixtures/file_tree1.json",
-        "tree_json2": "tests/fixtures/file_tree2.json",
-        "tree_yml1": "tests/fixtures/file_tree1.yml",
-        "tree_yml2": "tests/fixtures/file_tree2.yml",
+        "json1": os.path.join(fixtures_dir, "file1.json"),
+        "json2": os.path.join(fixtures_dir, "file2.json"),
+        "yml1": os.path.join(fixtures_dir, "file1.yml"),
+        "yml2": os.path.join(fixtures_dir, "file2.yml"),
+        "tree_json1": os.path.join(fixtures_dir, "file_tree1.json"),
+        "tree_json2": os.path.join(fixtures_dir, "file_tree2.json"),
+        "tree_yml1": os.path.join(fixtures_dir, "file_tree1.yml"),
+        "tree_yml2": os.path.join(fixtures_dir, "file_tree2.yml"),
     }
 
 
@@ -39,10 +41,10 @@ def test_generate_diff(paths, format_name):
     result_diff_yml_tree_stat = generate_diff(paths["tree_yml1"], paths["tree_yml2"], format_name["plain"])
     result_diff_json_view = generate_diff(paths["tree_json1"], paths["tree_json2"], format_name["json"])
 
-    assert result_diff_json == read_file_content('tests/fixtures/stylish_plain')
-    assert result_diff_yml == read_file_content('tests/fixtures/stylish_plain')
-    assert result_diff_json_tree == read_file_content('tests/fixtures/stylish')
-    assert result_diff_yml_tree == read_file_content('tests/fixtures/stylish')
-    assert result_diff_json_tree_stat == read_file_content('tests/fixtures/plain')
-    assert result_diff_yml_tree_stat == read_file_content('tests/fixtures/plain')
-    assert result_diff_json_view == read_file_content('tests/fixtures/json')
+    assert result_diff_json == read_file_content(os.path.join('tests/fixtures', 'stylish_plain'))
+    assert result_diff_yml == read_file_content(os.path.join('tests/fixtures', 'stylish_plain'))
+    assert result_diff_json_tree == read_file_content(os.path.join('tests/fixtures', 'stylish'))
+    assert result_diff_yml_tree == read_file_content(os.path.join('tests/fixtures', 'stylish'))
+    assert result_diff_json_tree_stat == read_file_content(os.path.join('tests/fixtures', 'plain'))
+    assert result_diff_yml_tree_stat == read_file_content(os.path.join('tests/fixtures', 'plain'))
+    assert result_diff_json_view == read_file_content(os.path.join('tests/fixtures', 'json'))
